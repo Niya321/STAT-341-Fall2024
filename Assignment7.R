10274d95f9daa5bfd685a4f79a549f9a9f88791d
Niya321 <111890898+Niya321@users.noreply.github.com>
2024-10-31 15:58:39 -0400

    Assignment7.R

--- Assignment7.R
+++ Assignment7.R
@@ -12,3 +12,14 @@ lines(density(x),col='green')
 lines(density(y), col='blue')
 Apply the Ttest function to get P value
 ttest = t.test(x,y)
+
+	Welch Two Sample t-test
+
+data:  x and y
+t = -0.62247, df = 506.05, p-value = 0.5339
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -0.1644355  0.0853086
+sample estimates:
+  mean of x   mean of y 
+0.004964016 0.044527485 
